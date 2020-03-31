f868833cd602d34355ba9619a4fe7c38af028d33
Max Ruttenberg <mrutt@cs.washington.edu>
2020-03-31 11:48:54 -0700

    adds vertexset class

---
+++ VertexSet.hpp
@@ -0,0 +1,46 @@
+#include <Graph.hpp>
+#include <set>
+#include <functional>
+#include <cstdlib>
+namespace graph_tools {
+
+    using VertexSet = std::set<Graph::NodeID>;
+
+    class VertexSetBuilder {
+    public:
+        virtual VertexSet build(const Graph & graph) = 0;
+    };
+
+    class VertexSetBuilderBase : public VertexSetBuilder {
+    public:
+        VertexSet build(const Graph & graph) {
+            VertexSet set;
+            while (!done(graph, set)) insert_next_vertex(graph, set);
+            return set;
+        }
+
+        virtual void insert_next_vertex(const Graph &graph, VertexSet &set) = 0;
+        virtual bool done(const Graph &graph, const VertexSet &set) const = 0;
+    };
+
+    template <typename DoneFunction>
+    class RandomVertexSetBuilder final : public VertexSetBuilderBase {
+    public:
+        RandomVertexSetBuilder(DoneFunction done_f):
+            _done_f(done_f) {
+            srand(0);
+        }
+        void insert_next_vertex(const Graph &graph, VertexSet &set) {
+            set.insert(rand()%graph.num_nodes());
+        }
+        bool done (const Graph& graph, const VertexSet &set ) const {
+            return _done_f(graph, set);
+        }
+
+    private:
+        DoneFunction _done_f;
+    };
+
+    class BFSVertexSetBuilder : public VertexSetBuilderBase {
+    };
+}
