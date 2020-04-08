6156631f36c0d17ac177f060a868118ee4d19c90
Max Ruttenberg <mchezrutt@gmail.com>
2020-04-08 14:00:38 -0700

    fixes makefiles for osx

--- libgenerator.mk
+++ libgenerator.mk
@@ -6,25 +6,35 @@ generator-dir  ?= $(graphtools-dir)/graph500/generator
 libgenerator.so-sources := $(filter-out %/mrg_transitions.c,$(wildcard $(generator-dir)/*.c))
 libgenerator.so-headers := $(wildcard $(generator-dir)/*.h)
 
+# ldflags for executables and libraries that use libgenerator
 libgenerator-interface-ldflags += -L$(graphtools-dir)
+ifeq ($(shell uname),Darwin)
+libgenerator-interface-ldflags += -Wl,-rpath,$(graphtools-dir)
+else
 libgenerator-interface-ldflags += -Wl,-rpath=$(graphtools-dir)
+libgenerator-interface-ldflags += -lgomp
+endif
 libgenerator-interface-ldflags += -lgenerator
 
+# cxxflags for executables and libraries that use libgenerator
 libgenerator-interface-cxxflags += -I$(generator-dir)
+
+# cxxflags for compiling libgenerator.so
 libgenerator-cxxflags += -fPIC
 libgenerator-cxxflags += -Drestrict=__restrict__
 libgenerator-cxxflags += -shared
 libgenerator-cxxflags += -O3
-
 ifeq ($(shell uname),Darwin)
 # MacOS
 else
 # Linux
 libgenerator-cxxflags += -fopenmp
-libgenerator-interface-ldflags += -lgomp
 endif
 
+# libgraphstools header - for dependencies
 libgenerator-interface-headers := $(libgenerator.so-headers)
+
+# libgenerator libraries - for dependencies
 libgenerator-interface-libraries := $(graphtools-dir)/libgenerator.so
 
 $(graphtools-dir)/libgenerator.so: $(libgenerator.so-headers)
--- libgraphtools.mk
+++ libgraphtools.mk
@@ -11,28 +11,46 @@ libgraphtools.so-sources += $(graphtools-dir)/Graph.cpp
 libgraphtools.so-headers := $(wildcard $(graphtools-dir)/*.hpp)
 libgraphtools.so-objects := $(notdir $(libgraphtools.so-sources:.cpp=.o))
 
+# ldflags for executables and libraries that use libgraphtools
 libgraphtools-interface-ldflags += $(libgenerator-interface-ldflags)
 libgraphtools-interface-ldflags += -L$(graphtools-dir)
-libgraphtools-interface-ldflags += -Wl,-rpath=$(graphtools-dir)
 libgraphtools-interface-ldflags += -lgraphtools -lboost_serialization
+ifeq ($(shell uname),Darwin)
+libgraphtools-interface-ldflags += -Wl,-rpath,$(graphtools-dir)
+else
+libgraphtools-interface-ldflags += -Wl,-rpath=$(graphtools-dir)
+endif
 
+# ldflags for linking libgraphstools.so
+libgraphtools-ldflags += $(libgenerator-interface-ldflags)
+libgraphtools-ldflags += -shared
+libgraphtools-ldflags += -lboost_serialization
+
+# cxxflags for executables and libraries that use libgraphtools
 libgraphtools-interface-cxxflags += $(libgenerator-interface-cxxflags)
 libgraphtools-interface-cxxflags += -I$(graphtools-dir)
 libgraphtools-interface-cxxflags += -std=c++11
 
+# cxxflags for compiling libgraphtools.so
+libgraphtools-cxxflags += $(libgenerator-interface-cxxflags)
+libgraphtools-cxxflags += -I$(graphtools-dir)
+libgraphtools-cxxflags += -std=c++11
+
+# libgraphstools header - for dependencies
 libgraphtools-interface-headers += $(libgenerator-interface-headers)
 libgraphtools-interface-headers += $(libgraphtools.so-headers)
 
+# libgraphtools libraries - for dependencies
 libgraphtools-interface-libraries += $(libgenerator-interface-libraries)
 libgraphtools-interface-libraries += $(graphtools-dir)/libgraphtools.so
 
 $(libgraphtools.so-objects): $(libgenerator.so-headers)
 $(libgraphtools.so-objects): $(libgraphtools.so-headers)
 $(libgraphtools.so-objects): %.o: $(graphtools-dir)/%.cpp
-	$(CXX) -fPIC $(libgraphtools-interface-cxxflags) -c $<
+	$(CXX) -fPIC $(libgraphtools-cxxflags) -c $<
 
 $(graphtools-dir)/libgraphtools.so: $(libgraphtools.so-objects)
-	$(CXX) -shared -o $@ $^
+	$(CXX) $(libgraphtools-ldflags) -o $@ $^
 
 .PHONY: libgraphtools.so-clean
 libgraphtools.so-clean:
