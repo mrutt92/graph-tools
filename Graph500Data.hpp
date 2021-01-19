9544e8ca7c700d04d74fcf868b867b9ea953f6a0
Max Ruttenberg <mrutt@cs.washington.edu>
2021-01-19 08:45:00 -0800

    Adds a FromASCIIFile initializer

--- Graph500Data.hpp
+++ Graph500Data.hpp
@@ -61,6 +61,47 @@ namespace graph_tools {
             ofs.write(reinterpret_cast<char*>(_edges), sizeof(*_edges) * _nedges);
         }
 
+        static Graph500Data FromASCIIFile(const std::string & file_name) {
+            struct stat st;
+            int err;
+
+            // stat the file
+            if ((err = stat(file_name.c_str(), &st)) != 0) {
+                std::string errm(strerror(errno));
+                throw std::runtime_error("Failed to stat '"
+                                         + file_name
+                                         + "': "
+                                         + errm);
+            }
+
+            // open using stdio
+            FILE *f = fopen(file_name.c_str(), "rb");
+            if (f == NULL) {
+                std::string errm(strerror(errno));
+                throw std::runtime_error("Failed to open '"
+                                         + file_name
+                                         + "': "
+                                         + errm);
+            }
+
+            // read each line
+            std::vector<packed_edge> edges;
+            int64_t v0, v1;
+            while (fscanf(f, "%" SCNd64 " %" SCNd64 "", &v0, &v1) == 2) {
+                packed_edge e;
+                write_edge(&e, v0, v1);
+                edges.push_back(e);
+            }
+
+            packed_edge *e = (packed_edge*)malloc(sizeof(*e) * edges.size());
+            if (!e)
+                throw std::bad_alloc();
+
+            memcpy(e, &edges[0], sizeof(*e) * edges.size());
+            Graph500Data data(e, edges.size());
+            return data;
+        }
+
         static Graph500Data FromFile(const std::string & file_name) {
             struct stat st;
             int err;
