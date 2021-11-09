40b310a8843d403b1cbf250b45ce4cacba9aea40
zy383 <zy383@cornell.edu>
2021-11-08 21:12:42 -0500

    For WGraph.hpp, add member functions supporting reading from CSR/CSC txt files For SparsePushBFS.hpp, add RunBFS member function that only store a single frontier information Add SparsePullBFS.hpp to compute pull direction stats

--- WGraph.hpp
+++ WGraph.hpp
@@ -16,6 +16,10 @@
 #include <string>
 #include <sstream>
 #include <random>
+
+#define FILENAME_SZ 1024
+#define PARA_SZ 16
+
 namespace graph_tools {
 
     class WGraph {
@@ -230,6 +234,482 @@ namespace graph_tools {
             return g;
         }
 
+        /* Build from CSR TXT Files */
+        // helper function
+        static void read_file_float(char* filename, float* buffer, char* mat_sz_m,char* mat_sz_n,char* mat_sz_nnz, char* density_str) {
+          FILE* file = fopen(filename, "r");
+          char* line_buf;
+          size_t line_buf_size = 0;
+          ssize_t line_size;
+          int line_cnt = 0;
+          // check whether the file canbe open
+          if (!file) {
+            printf("Can't open file: '%s'", filename);
+          }
+          // get the first line 
+          line_size = getline(&line_buf, &line_buf_size, file);
+          while (line_size >= 0) {
+            if (line_cnt == 0) {
+              for (int i = 0; i < PARA_SZ; i++) {
+                mat_sz_m[i] = line_buf[i];
+              }
+            }
+            else if (line_cnt == 1) {
+              for (int i = 0; i < PARA_SZ; i++) {
+                mat_sz_n[i] = line_buf[i];
+              }
+            }
+            else if (line_cnt == 3) {
+              for (int i = 0; i < PARA_SZ; i++) {
+                density_str[i] = line_buf[i];
+                //printf("current lincnt is %d, while line buf is: %s",line_cnt,line_buf);
+              }
+            }
+            else if (line_cnt == 4) {
+              for (int i = 0; i < PARA_SZ; i++) {
+                mat_sz_nnz[i] = line_buf[i];
+              }
+            }
+            buffer[line_cnt] = atof(line_buf);
+            line_cnt++;
+            /* Show the line details */
+            //printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_cnt, line_size, line_buf_size, line_buf);
+
+            /* Get the next line */
+            line_size = getline(&line_buf, &line_buf_size, file);
+          }
+
+          free(line_buf);
+          line_buf = NULL;  
+          // close the file
+          fclose(file);
+        }
+
+        static void read_file_int(char* filename, int* buffer) {
+          FILE* file = fopen(filename, "r");
+          char* line_buf;
+          size_t line_buf_size = 0;
+          ssize_t line_size;
+          int line_cnt = 0;
+          // check whether the file canbe open
+          if (!file) {
+            printf("Can't open file: '%s'", filename);
+          }
+          // get the first line 
+          line_size = getline(&line_buf, &line_buf_size, file);
+          while (line_size >= 0) {
+            buffer[line_cnt] = atoi(line_buf);
+            line_cnt++;
+            /* Show the line details */
+            //printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_cnt, line_size, line_buf_size, line_buf);
+
+            /* Get the next line */
+            line_size = getline(&line_buf, &line_buf_size, file);
+          }
+
+          free(line_buf);
+          line_buf = NULL;  
+          // close the file
+          fclose(file);
+        }
+
+        
+
+
+        static WGraph FromCSR(const std::string & file_name,const std::string & file_path, int ite, int num_pods) {
+        const char* filename_base ;
+        filename_base = file_path.c_str();
+        char filename_info[FILENAME_SZ];
+        // concat the string
+        strcpy(filename_info, filename_base);
+        strcat(filename_info, "spm_tb_info.dat");
+        float tb_info[5];
+        char  mat_m_str[PARA_SZ];
+        char  mat_n_str[PARA_SZ];
+        char  mat_nnz_str[PARA_SZ];
+        char  density_str[PARA_SZ];
+        read_file_float(filename_info, tb_info, mat_m_str,mat_n_str,mat_nnz_str, density_str);
+        // parameters
+	    int mat_sz = 1024;
+	    int m = atoi(mat_m_str);
+	    int n = atoi(mat_n_str);
+        int A_len  = atoi(mat_nnz_str);
+        //for (int i = 0; i < 5; i++) {
+        //    printf("tb_info[%d]: %f \n", i, tb_info[i]);
+        //}
+        
+        // allocate memory for graph
+        int* A_idx = (int*) malloc (A_len*sizeof(int));
+        int* A_ptr = (int*) malloc ((m+1)*sizeof(int));
+        
+        
+        char filename_b[FILENAME_SZ];
+        strcpy(filename_b, filename_base);
+        strcat(filename_b, file_name.c_str());
+        
+        //printf("Test benchmark: %s", filename_b);
+        //printf("Reading A_val...............................................................\n");
+        char f_A_idx[FILENAME_SZ];
+        strcpy(f_A_idx, filename_b);
+        strcat(f_A_idx, "_A_idx.dat");
+        read_file_int(f_A_idx, A_idx);
+
+        char f_A_ptr[FILENAME_SZ];
+        strcpy(f_A_ptr, filename_b);
+        strcat(f_A_ptr, "_A_ptr.dat");
+        read_file_int(f_A_ptr, A_ptr);
+
+        std::vector<NodeID> degree;
+        std::vector<NodeID> neighbors;
+        std::vector<NodeID> offsets;
+        NodeID offset_cnt = 0;
+        for(int i=ite; i<m; i += num_pods){
+            degree.push_back(A_ptr[i+1]-A_ptr[i]);
+            for(int j=A_ptr[i]; j<A_ptr[i+1]; j++){
+                neighbors.push_back(A_idx[j]);   
+            }
+            offsets.push_back(offset_cnt);
+            offset_cnt += A_ptr[i+1]-A_ptr[i];
+        }
+
+
+        
+        //printf("++++++++++++++++++++++++++++size of degree: %d",degree.size());
+
+        WGraph g;
+        g._neighbors = std::move(neighbors);
+        g._offsets   = std::move(offsets);
+        g._degrees   = std::move(degree);
+
+        free(A_idx);
+        free(A_ptr);
+
+        std::vector<float> weights(offset_cnt);
+        std::uniform_real_distribution<float> dist(0.99,1.01);
+        std::uniform_int_distribution<int> idist(0, m-1);
+        std::default_random_engine gen;
+
+        for (int i = 0; i < offset_cnt; i++)
+            weights[i] = dist(gen);
+
+        g._weights = std::move(weights);
+        
+        return g;
+        
+        }  
+
+        static WGraph FromCSC(const std::string & file_name,const std::string & file_path, int ite, int num_pods) {
+        const char* filename_base ;
+        filename_base = file_path.c_str();
+        char filename_info[FILENAME_SZ];
+        // concat the string
+        strcpy(filename_info, filename_base);
+        strcat(filename_info, "spm_tb_info.dat");
+        float tb_info[5];
+        char  mat_m_str[PARA_SZ];
+        char  mat_n_str[PARA_SZ];
+        char  mat_nnz_str[PARA_SZ];
+        char  density_str[PARA_SZ];
+        read_file_float(filename_info, tb_info, mat_m_str,mat_n_str,mat_nnz_str, density_str);
+        // parameters
+	    int mat_sz = 1024;
+	    int m = atoi(mat_m_str);
+	    int n = atoi(mat_n_str);
+        int A_len  = atoi(mat_nnz_str);
+        //for (int i = 0; i < 5; i++) {
+        //    printf("tb_info[%d]: %f \n", i, tb_info[i]);
+        //}
+        
+        // allocate memory for graph
+        int* A_idx = (int*) malloc (A_len*sizeof(int));
+        int* A_ptr = (int*) malloc ((m+1)*sizeof(int));
+        
+        
+        char filename_b[FILENAME_SZ];
+        strcpy(filename_b, filename_base);
+        strcat(filename_b, file_name.c_str());
+        
+        //printf("Test benchmark: %s", filename_b);
+        //printf("Reading A_val...............................................................\n");
+        char f_A_idx[FILENAME_SZ];
+        strcpy(f_A_idx, filename_b);
+        strcat(f_A_idx, "_A_idx.dat");
+        read_file_int(f_A_idx, A_idx);
+
+        char f_A_ptr[FILENAME_SZ];
+        strcpy(f_A_ptr, filename_b);
+        strcat(f_A_ptr, "_A_ptr.dat");
+        read_file_int(f_A_ptr, A_ptr);
+
+        std::vector<NodeID> degree;
+        std::vector<NodeID> neighbors;
+        std::vector<NodeID> offsets;
+        NodeID offset_cnt = 0;
+        for(int i=0; i<n; i ++){
+            NodeID degree_col = 0;
+            for(int j=A_ptr[i]; j<A_ptr[i+1]; j++){
+                if(A_idx[j]%num_pods==ite){
+                    neighbors.push_back(A_idx[j]/num_pods); 
+                    degree_col ++;   
+                }
+            }
+            degree.push_back(degree_col);
+            offsets.push_back(offset_cnt);
+            offset_cnt += degree_col;
+        }
+        //number of nodes will be n!
+
+
+        
+        //printf("++++++++++++++++++++++++++++size of degree: %d",degree.size());
+
+        WGraph g;
+        g._neighbors = std::move(neighbors);
+        g._offsets   = std::move(offsets);
+        g._degrees   = std::move(degree);
+
+        free(A_idx);
+        free(A_ptr);
+
+        std::vector<float> weights(offset_cnt);
+        std::uniform_real_distribution<float> dist(0.99,1.01);
+        std::uniform_int_distribution<int> idist(0, m-1);
+        std::default_random_engine gen;
+
+        for (int i = 0; i < offset_cnt; i++)
+            weights[i] = dist(gen);
+
+        g._weights = std::move(weights);
+        
+        return g;
+        
+        }  
+
+        static WGraph FromCSR(const std::string & file_name,const std::string & file_path) {
+        const char* filename_base ;
+        filename_base = file_path.c_str();
+        char filename_info[FILENAME_SZ];
+        // concat the string
+        strcpy(filename_info, filename_base);
+        strcat(filename_info, "spm_tb_info.dat");
+        float tb_info[5];
+        char  mat_m_str[PARA_SZ];
+        char  mat_n_str[PARA_SZ];
+        char  mat_nnz_str[PARA_SZ];
+        char  density_str[PARA_SZ];
+        read_file_float(filename_info, tb_info, mat_m_str,mat_n_str,mat_nnz_str, density_str);
+        // parameters
+	    int mat_sz = 1024;
+	    int m = atoi(mat_m_str);
+	    int n = atoi(mat_n_str);
+        int A_len  = atoi(mat_nnz_str);
+        //for (int i = 0; i < 5; i++) {
+        //    printf("tb_info[%d]: %f \n", i, tb_info[i]);
+        //}
+        
+        // allocate memory for graph
+        int* A_idx = (int*) malloc (A_len*sizeof(int));
+        int* A_ptr = (int*) malloc ((m+1)*sizeof(int));
+
+
+        
+        
+        char filename_b[FILENAME_SZ];
+        strcpy(filename_b, filename_base);
+        strcat(filename_b, file_name.c_str());
+        
+        //printf("Test benchmark: %s", filename_b);
+        //printf("Reading A_val...............................................................\n");
+        char f_A_idx[FILENAME_SZ];
+        strcpy(f_A_idx, filename_b);
+        strcat(f_A_idx, "_A_idx.dat");
+        read_file_int(f_A_idx, A_idx);
+
+        char f_A_ptr[FILENAME_SZ];
+        strcpy(f_A_ptr, filename_b);
+        strcat(f_A_ptr, "_A_ptr.dat");
+        read_file_int(f_A_ptr, A_ptr);
+
+
+        std::vector<NodeID> neighbors;
+        for(int i=0; i<A_len; i++){
+            neighbors.push_back(A_idx[i]);
+        }
+
+        std::vector<NodeID> offsets;
+        for(int i=0; i<m; i++){
+            offsets.push_back(A_ptr[i]);
+        }
+
+        std::vector<NodeID> degree;
+        for(int i=0; i<m; i++){
+            degree.push_back(A_ptr[i+1]-A_ptr[i]);
+        }
+        printf("++++++++++++++++++++++++++++size of degree: %d",degree.size());
+
+        WGraph g;
+        g._neighbors = std::move(neighbors);
+        g._offsets   = std::move(offsets);
+        g._degrees   = std::move(degree);
+
+        free(A_idx);
+        free(A_ptr);
+
+        std::vector<float> weights(A_len);
+        std::uniform_real_distribution<float> dist(0.99,1.01);
+        std::uniform_int_distribution<int> idist(0, m-1);
+        std::default_random_engine gen;
+
+        for (int i = 0; i < A_len; i++)
+            weights[i] = dist(gen);
+
+        g._weights = std::move(weights);
+        
+        return g;
+        
+        
+        
+        }   
+
+        static WGraph C2SR(const std::string & file_name,const std::string & file_path) {
+        const char* filename_base ;
+        filename_base = file_path.c_str();
+        char filename_info[FILENAME_SZ];
+        // concat the string
+        strcpy(filename_info, filename_base);
+        strcat(filename_info, "spm_tb_info.dat");
+        float tb_info[5];
+        char  mat_m_str[PARA_SZ];
+        char  mat_n_str[PARA_SZ];
+        char  mat_nnz_str[PARA_SZ];
+        char  density_str[PARA_SZ];
+        read_file_float(filename_info, tb_info, mat_m_str,mat_n_str,mat_nnz_str, density_str);
+        // parameters
+	    int mat_sz = 1024;
+	    int m = atoi(mat_m_str);
+	    int n = atoi(mat_n_str);
+        int A_len  = atoi(mat_nnz_str);
+        //for (int i = 0; i < 5; i++) {
+        //    printf("tb_info[%d]: %f \n", i, tb_info[i]);
+        //}
+        
+        // allocate memory for graph
+        int* A_idx = (int*) malloc (A_len*sizeof(int));
+        int* A_ptr = (int*) malloc ((m+1)*sizeof(int));
+
+
+        
+        
+        char filename_b[FILENAME_SZ];
+        strcpy(filename_b, filename_base);
+        strcat(filename_b, file_name.c_str());
+        
+        //printf("Test benchmark: %s", filename_b);
+        //printf("Reading A_val...............................................................\n");
+        char f_A_idx[FILENAME_SZ];
+        strcpy(f_A_idx, filename_b);
+        strcat(f_A_idx, "_A_idx.dat");
+        read_file_int(f_A_idx, A_idx);
+
+        char f_A_ptr[FILENAME_SZ];
+        strcpy(f_A_ptr, filename_b);
+        strcat(f_A_ptr, "_A_ptr.dat");
+        read_file_int(f_A_ptr, A_ptr);
+
+
+        //set 32 vectors to sort neighbours into 32 banks
+        std::vector<NodeID> bank_contents[32];
+        //set 32 variables to record the ptr of each row in the bank_contents
+        int local_ptr[32];
+        for(int i=0; i<32; i++){
+            local_ptr[i] = 0;
+        }
+        //declare the offset vector
+        std::vector<NodeID> offsets;
+        std::vector<NodeID> degree;
+        for(int i=0; i<m; i++){
+            for(int j=A_ptr[i]; j<A_ptr[i+1]; j++){
+                bank_contents[i%32].push_back(A_idx[j]);
+                //if(i==829558){
+                //    std::cout<<"======================= node 829558's edge pushed: "<<A_idx[j]<<"============================"<<std::endl;
+                //}
+            }
+            
+            //update offset after store one row    
+            offsets.push_back((i%32)*32+(local_ptr[i%32]/32)*1024+(local_ptr[i%32]%32));
+            //if(i==829558){
+                //std::cout<<"======================= node 829558's offset pushed: "<<offsets.back()<<"============================"<<std::endl;
+                //std::cout<<"======================= node 829558's local ptr: "<<local_ptr[i%32]<<"============================"<<std::endl;
+            //}
+            local_ptr[i%32] += A_ptr[i+1]- A_ptr[i]; 
+            degree.push_back(A_ptr[i+1]-A_ptr[i]);     
+        }
+        
+        std::vector<NodeID> neighbors;
+        //construct the neighbors vector
+        uint32_t remain = 0;
+        while(1){
+            //iterate over 32 bank_content vectors
+            for(int i=0; i<32; i++){
+                //iterate over 32 words per bank
+                for(int j=0; j<32; j++){
+                    if(bank_contents[i].size()>0){
+                        neighbors.push_back(bank_contents[i].front());
+                        bank_contents[i].erase(bank_contents[i].begin());
+                    }
+                    else{
+                        neighbors.push_back(0); 
+                    }
+                }
+            }
+            for(int j=0; j<32; j++){
+                remain += bank_contents[j].size();
+            }
+            if(remain == 0)
+                break;
+            else
+                remain = 0;
+        }
+        
+        //debug
+        /*int ptr_829558 = offsets[829558];
+        std::cout<<"======================= node 829558's offset: "<<ptr_829558<<"============================"<<std::endl;
+        int degree_829558 = degree[829558];
+        std::cout<<"======================= node 829558's degree: "<<degree_829558<<"============================"<<std::endl;
+        for(int i=0; i<degree_829558; i++){
+            std::cout<<"======================= node 829558's edge "<<i<<" is: "<<neighbors[ptr_829558]<<"offset is "<<ptr_829558<<"============================"<<std::endl;    
+            ptr_829558 += 1;
+            if(ptr_829558%32==0){
+                ptr_829558 += 992;
+            }
+        }*/
+
+
+
+
+        WGraph g;
+        g._neighbors = std::move(neighbors);
+        g._offsets   = std::move(offsets);
+        g._degrees   = std::move(degree);
+
+        free(A_idx);
+        free(A_ptr);
+
+        std::vector<float> weights(A_len);
+        std::uniform_real_distribution<float> dist(0.99,1.01);
+        std::uniform_int_distribution<int> idist(0, m-1);
+        std::default_random_engine gen;
+
+        for (int i = 0; i < A_len; i++)
+            weights[i] = dist(gen);
+
+        g._weights = std::move(weights);
+        
+        return g;
+        
+        
+        
+        }   
         /**
          * Graph with uniform degree
          */
